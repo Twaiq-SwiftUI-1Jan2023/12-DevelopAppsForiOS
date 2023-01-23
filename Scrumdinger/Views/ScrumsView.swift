5abdab3ca8f00286a7955d048b87c88a3033f55c
amer266030 <amer266030@users.noreply.github.com>
2023-01-23 20:39:37 +0300

    Amer's Commit

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,39 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Amer Alyusuf on 23/01/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button {} label: {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
+    }
+}
