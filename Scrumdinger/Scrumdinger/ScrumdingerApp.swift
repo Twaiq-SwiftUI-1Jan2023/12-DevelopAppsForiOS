d51538d4ac5ec0fb6091d525fd849977199d5585
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2023-01-24 00:24:42 +0300

    'CardView' that displays a summary of a daily scrum

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Raneem AlRashoud on 02/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
