295527066416e5b0464d2c1e0c404c4cedc4bcbf
Abdulkarim Koshak <abdulkarim.koshak@gmail.com>
2023-01-24 03:04:33 +0300

    Added solution files

---
+++ Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Abdulkarim Koshak on 1/24/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
